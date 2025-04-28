c59a4e2ecf47fcd408071b32c1ba86e772b8760e
Ilona Tykhonovskaya <ilonatiho@gmail.com>
2025-04-28 11:52:20 +0200

    exercices 1 et 2 function js

---
+++ Exercice 5 23.04/fonction saluerUtilisateur.js
@@ -0,0 +1,45 @@
+// Affichage du message 
+function saluerUtilisateur() {
+
+    let prenom = "Ive";
+    let nom = "Lbn";
+
+
+}
+alert(`Bonjour ${prenom} .`);
+// saluerUtilisateur()
+
+function addition(a, b) {
+    let a = 8;
+    let b = 3;
+    return a + b;
+
+    const resultat = addition(8, 3);
+    console.log(resultat); // Affichera 11 dans la console
+}
+
+// addition(a, b)
+
+function estPair(nombre) {
+    return nombre % 2 === 0;
+
+
+    // Tests de la fonction
+    console.log(estPair(2));  // true
+    console.log(estPair(7));  // false
+    console.log(estPair(0));  // true
+    console.log(estPair(15)); // false
+    console.log(estPair(28)); // true
+}
+// estPair(nombre)
+
+function calculerMoyenne() {
+    return (1 + 2 + 3) / 3;
+
+
+    // Exemple d'utilisation
+    const moyenne = calculerMoyenne(1, 2, 3);
+    console.log("La moyenne est : " + moyenne);
+}
+calculerMoyenne()
+
---
+++ Exercice 5 23.04/function revelerAnimal(magie).js
@@ -0,0 +1,25 @@
+function revelerAnimal(magie) {
+    if (magie === 1) {
+        alert("Ton animal magique est une Licorne ");
+    } else if (magie === 2) {
+        alert("Ton animal magique est une Grenouille ");
+    } else if (magie === 3) {
+        alert("Ton animal magique est un Dragon ");
+    } else if (magie === 4) {
+        alert("Ton animal magique est un Hibou ");
+    } else if (magie === 5) {
+        alert("Ton animal magique est un Escargot ");
+    } else {
+        alert("Chiffre inconnu : tu es trop puissant pour être classé dans notre bestiaire !");
+    }
+}
+let choix = prompt("Choisis un chiffre entre 1 et 5 pour découvrir ton animal magique :");
+choix = Number(choix);
+
+revelerAnimal(choix);
+
+while (isNaN(choix)); // Redemander si ce n'est pas un nombre
+
+
+// Message final
+alert("Merci d'avoir joué au jeu du bestiaire magique !");
